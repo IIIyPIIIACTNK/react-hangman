b4dc202cee5549c4bf6b9b45b8c89e106a641984
alexy-shu <alexey.shustrov@gengo.online>
2024-07-18 12:39:26 +0300

    page + routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react"
 import { useState } from "react"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Store from "./pages/Store"
+import About from "./pages/About"
+
 
 function App(){
-  const [wordToGuess, setWordToGuess] = useState("test")
-  return <h1>Hi</h1>
+  return <div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/store" element={<Store/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
+
+  </div>
 }
 
 export default App
